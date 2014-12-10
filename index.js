8f45084b123a49de37b53ee5418219b8fe1b2f40
pandafulmanda <pandafulmanda@gmail.com>
2014-12-10 17:47:15 -0600

    twitter as an input

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 'use strict';
 
+// require packages
 var five = require('johnny-five'),
   express = require('express'),
   socket = require('socket.io'),
 
+  // load output and input modules
   lightBulb = require('./light'),
   socketBot = require('./socketBot'),
+  twitterInput = require('./twitter'),
 
+  // set the server and socket
   app = express(),
   server = require('http').createServer(app),
   io = socket(server),
 
+  // set up sockets
   lightBulbSocket = io.of('/light-bulb'),
   socketBotSocket = io.of('/sock-bot'),
+
+  // make a new board to interface with the Arduino
   board = new five.Board();
 
 
 lightBulb.connectBoard(board);
 lightBulb.connectSocket(lightBulbSocket);
 
-
 socketBot.connectBoard(board);
 socketBot.connectSocket(socketBotSocket);
 
+twitterInput.connectInputSocket();
 
+// serve up files from the client folder
 app.use(express.static(__dirname + '/client'));
 
+// set up server on port 3000
 server = server.listen(3000, function(){
-
   console.log('Yo dawg, yo server is running');
-
 });
--- twitter/index.js
+++ twitter/index.js
@@ -4,9 +4,8 @@ var io = require('socket.io-client'),
 
   twitterKeys, twit, lightBulbSocket;
 
-// load environment variables
+// load environment variables into Twitter keys
 dotenv.load();
-
 twitterKeys = {
   consumer_key: process.env.TWITTER_KEY,
   consumer_secret: process.env.TWITTER_SECRET,
@@ -14,9 +13,20 @@ twitterKeys = {
   access_token_secret: process.env.TWITTER_ACCESS_SECRET
 };
 
+// connect Twitter to configured API
 twit = new twitter(twitterKeys);
 
-exports.connectSocket = function(){
-  lightBulbSocket = io('/light-bulb');
-  lightBulbSocket.emit('switch');
+
+exports.connectInputSocket = function(){
+
+  lightBulbSocket = io('http://localhost:3000/light-bulb');
+
+  twit.stream('statuses/filter', {track: ['EricGarner', 'HoustonJS']}, function(stream) {
+      stream.on('data', function(data) {
+        if(!data.retweeted_status){
+          lightBulbSocket.emit('switch');
+        }
+      });
+  });
+
 };
