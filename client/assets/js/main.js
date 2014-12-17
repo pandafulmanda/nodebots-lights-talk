c64fef680e42b86ce274c5c018f583163400bdca
pandafulmanda <pandafulmanda@gmail.com>
2014-12-17 07:57:43 -0600

    lets focus on lights only

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,58 +1,11 @@
 (function(){
 
-  var lightBulbSocket = io('/light-bulb'),
-    sockBot = io('/sock-bot'),
-
-    botDirectionKeys = {
-      '37' : 'left',
-      '38' : 'forward',
-      '39' : 'right',
-      '40' : 'backward'
-    },
-    driveBotEmit = _.debounce( _startDriving, 100, {leading: true, trailing: false}),
-    stopBotEmit = _.debounce( _stopDriving, 100, {leading: false});
-
+  var lightBulbSocket = io('/light-bulb');
 
   // whenever something is clicked or touched
-  document.addEventListener('mousedown', function(event){
-    if(event.target === event.currentTarget){
-      event.stopPropagation();
-      return;
-    }
-
-    if(event.target.dataset.light){
-      toggleLight([event.target.dataset.light]);
-    }
-
-    if(event.target.dataset.bot){
-      driveBotEmit(event.target.dataset.bot);
-    }
-  });
+  document.addEventListener('mousedown', handleButtonStart);
   // handleButtonStart is the same as the function on mousedown above.
   document.addEventListener('touchstart', handleButtonStart);
-
-  // On button press end, the robot 
-  document.addEventListener('mouseup', endRobotPressHandler);
-  document.addEventListener('touchend', endRobotPressHandler);
-  document.addEventListener('touchcancel', endRobotPressHandler);
-  document.addEventListener('touchleave', endRobotPressHandler);
-
-  window.onkeydown = function(keyEvent){
-
-    var direction = getDirection(keyEvent);
-
-    if(!direction){
-      return;
-    }
-
-    stopBotEmit(keyEvent);
-
-    if(keyEvent.repeat){      
-      driveBotEmit(direction);
-    }
-  };
-
-
 
 
   function handleButtonStart(event){
@@ -64,12 +17,7 @@
     if(event.target.dataset.light){
       toggleLight([event.target.dataset.light]);
     }
-
-    if(event.target.dataset.bot){
-      driveBotEmit(event.target.dataset.bot);
-    }
   }
-
 
 
   function toggleLight(light){
@@ -79,34 +27,5 @@
     lightBulbSocket.emit('switch', light);
   }
 
-
-
-  // Robot things
-  function getDirection(keyEvent){
-    return botDirectionKeys[keyEvent.which];
-  }
-
-  function endRobotPressHandler(pressEvent){
-    if(pressEvent.target === pressEvent.currentTarget){
-      pressEvent.stopPropagation();
-      return;
-    }
-
-    if(pressEvent.target.dataset.bot){
-      stopBotEmit(pressEvent, true);
-    }
-  }
-
-  // Both of the following are "debounced",
-  // meaning they can only happen once within the defined time.
-  function _startDriving(direction){
-    sockBot.emit('drive', direction);
-  }
-
-  function _stopDriving(keyEvent, force){
-    if(force || keyEvent.repeat){
-      sockBot.emit('stop');
-    }
-  }
 
 }());
--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 
 // require packages
 var five = require('johnny-five'),
-  Spark = require("spark-io"),
   express = require('express'),
   socket = require('socket.io'),
   dotenv = require('dotenv'),
 
   // load output and input modules
   lightBulb = require('./light'),
-  socketBot = require('./socketBot'),
   twitterInput = require('./twitter'),
 
   // set the server and socket
@@ -19,29 +17,16 @@ var five = require('johnny-five'),
 
   // set up sockets
   lightBulbSocket = io.of('/light-bulb'),
-  socketBotSocket = io.of('/sock-bot'),
 
   // make a new johnny five instance to interface with the Arduino
-  arduino = new five.Board(),
-
-  sparky;
+  arduino = new five.Board();
 
 // load environment variables
 dotenv.load();
 
-// make a new johnny five instance to interface with the spark core
-sparky = new five.Board({
-  io: new Spark({
-    token: process.env.SPARK_TOKEN,
-    deviceId: process.env.SPARK_DEVICE_ID
-  })
-});
 
 lightBulb.connectBoard(arduino);
 lightBulb.connectSocket(lightBulbSocket);
-
-socketBot.connectBoard(sparky);
-socketBot.connectSocket(socketBotSocket);
 
 twitterInput.connectInputSocket();
 
